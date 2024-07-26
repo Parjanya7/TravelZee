3500847ba0e278f4ca81e86d23a0f5066e7f79fd
Parjanya <57247224+Parjanya7@users.noreply.github.com>
2024-07-26 16:23:30 +0530

    Update FooterDataOne.js

--- src/constant/common/FooterDataOne.js
+++ src/constant/common/FooterDataOne.js
@@ -9,7 +9,7 @@ const FooterDataOne = {
   facebookLink: "/",
   youtubeLink: "/",
   copyrightText: "© TRAVELZEEWORLD 2024 | All rights reserved.",
-  phone: "+44 7480 784180",
+  phone: "+44 20 4600 3689",
   email: "admin@travelzeeworld.com",
   address: "B603 Sumeru heights Vanivad circle, Nadiad, Gujarat, India 387001",
 };
